bad0451d1aa5d304eb14819e6047379b845258fe
teyou1999 <yiyuemej@yahoo.fr>
2022-11-07 16:18:07 +0100

    exo 1: print list of students and the firt of the list

---
+++ ApplicationName/src/app/Student.ts
@@ -0,0 +1,8 @@
+export class Student {
+    id: number;
+    name: string;
+    sexe: string;
+    joinDate: Date;
+    age: number;
+    picture: string;
+}
---
+++ ApplicationName/src/app/mock-student-list.ts
@@ -0,0 +1,61 @@
+import { Student } from "./Student";
+
+export const STUDENTS: Student[] = [
+    {
+        id: 1,
+        name: "Oussama1 GOUNAYA",
+        sexe: "Homme",
+        joinDate: new Date(),
+        age: 26,
+        picture: "https://static.wikia.nocookie.net/disney/images/5/51/Giffany.png",
+    },{
+        id: 2,
+        name: "Oussama2 GOUNAYA",
+        sexe: "Homme",
+        joinDate: new Date(),
+        age: 26,
+        picture: "https://static.wikia.nocookie.net/disney/images/5/51/Giffany.png",
+    },{
+        id: 3,
+        name: "Oussama3 GOUNAYA",
+        sexe: "Homme",
+        joinDate: new Date(),
+        age: 26,
+        picture: "https://static.wikia.nocookie.net/disney/images/5/51/Giffany.png",
+    },{
+        id: 4,
+        name: "Oussama4 GOUNAYA",
+        sexe: "Homme",
+        joinDate: new Date(),
+        age: 26,
+        picture: "https://static.wikia.nocookie.net/disney/images/5/51/Giffany.png",
+    },{
+        id: 5,
+        name: "Oussama5 GOUNAYA",
+        sexe: "Homme",
+        joinDate: new Date(),
+        age: 26,
+        picture: "https://static.wikia.nocookie.net/disney/images/5/51/Giffany.png",
+    },{
+        id: 6,
+        name: "Oussama6 GOUNAYA",
+        sexe: "Homme",
+        joinDate: new Date(),
+        age: 26,
+        picture: "https://static.wikia.nocookie.net/disney/images/5/51/Giffany.png",
+    },{
+        id: 7,
+        name: "Oussama7 GOUNAYA",
+        sexe: "Homme",
+        joinDate: new Date(),
+        age: 26,
+        picture: "https://static.wikia.nocookie.net/disney/images/5/51/Giffany.png",
+    },{
+        id: 8,
+        name: "Oussama8 GOUNAYA",
+        sexe: "Homme",
+        joinDate: new Date(),
+        age: 26,
+        picture: "https://static.wikia.nocookie.net/disney/images/5/51/Giffany.png",
+    }
+]

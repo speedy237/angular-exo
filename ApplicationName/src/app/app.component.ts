1d60aaeb18d6a9a591c50f8afa1279463074ebe7
teyou1999 <yiyuemej@yahoo.fr>
2022-11-08 11:53:44 +0100

    exo 2 ngfor directive

--- ApplicationName/src/app/app.component.ts
+++ ApplicationName/src/app/app.component.ts
@@ -1,21 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import {Student} from './Student'
 import { STUDENTS } from './mock-student-list';
-
+import { Router } from '@angular/router';
+import {NgForm} from '@angular/forms'
 @Component({
   selector: 'app-root',
-  template: `
-    <h1> WELCOME JORDAN    </h1>`,
+  templateUrl: 'app-component.html',
   styles: []
 })
 export class AppComponent implements OnInit {
+  idStudent: number;
   studentList: Student[]=STUDENTS;
+  studentSelected: Student|undefined;
+  constructor(private router: Router) { }
    ngOnInit(): void {
       console.table(this.studentList);
       console.table(this.studentList[0].name);
    }
-   selectStudent=(studentName: string) => {
-    console.log(`vous avez cliquer sur l'etudiant  ${studentName}`)
+   selectStudent=(event: MouseEvent) => {
+    const index:number=+ (event.target as HTMLInputElement).value;
+    console.log(`vous avez clique sur l'etudiant ${this.studentList[index].name}`)
    }
-   
+   selectOneStudent(studentId: string) { 
+    console.log("youpi")
+    const student: Student|undefined = this.studentList.find(
+      student => student.id == +studentId
+    );
+     if(student) {
+      console.log(`vous avez cliquer sur l'etudiant ${student.name}`);
+      this.studentSelected=student;
+     }
+     else {
+      console.log("l'etudiant n'existe pas");
+     }
+
+  }
 }
